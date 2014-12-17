6cc47689d579b5a677faee115a9bcf387384c5e5
Mitch Souders <mitch.souders@gmail.com>
2014-12-16 23:27:36 -0800

    Cleaned up JSON output

--- imgur.py
+++ imgur.py
@@ -4,31 +4,36 @@ import urllib.request
 import zipfile
 import argparse
 import sys
+from collections import OrderedDict
 
 parser = argparse.ArgumentParser(description='Convert Imgur Gallery into a CKAN Flags Description.')
-parser.add_argument('imgur_url', help='imgur URL')
-parser.add_argument('ckan_file', nargs='?', type=argparse.FileType('w'), help='ckan output file', default=sys.stdout)
-parser.add_argument('--author', help='author name', default='Your Name Here')
+parser.add_argument('--author', help='author name', nargs='*', default=['Your Name Here'])
 parser.add_argument('--modname', help='mod name', default='Unnamed Flag Mod')
 parser.add_argument('--desc', help='description', default='Undescribed Flag Mod')
+parser.add_argument('imgur_url', help='imgur URL')
+parser.add_argument('ckan_file', nargs='?', type=argparse.FileType('w'), help='ckan output file', default=sys.stdout)
+
 args = parser.parse_args();
 
-ckan = {
-    'spec_version':1,
-    'identifier':args.modname.replace(" ", ""),
-    'name':args.modname,
-    'author':args.author,
-    'version':'0.1',
-    'abstract':args.desc,
-    'ksp_version':'any',
-}
+ckan = OrderedDict(
+    [
+        ('spec_version', 1),
+        ('identifier', args.modname.replace(" ", "")),
+        ('name', args.modname),
+        ('author', args.author),
+        ('version', '0.1'),
+        ('abstract', args.desc),
+        ('ksp_version', 'any'),
+    ]
+)
 
 ckan['resources'] = {'homepage':args.imgur_url}
-file_name, headers = urllib.request.urlretrieve(args.imgur_url + '/zip')
+ckan['download'] = args.imgur_url + '/zip'
+file_name, headers = urllib.request.urlretrieve(ckan['download'])
 
 imgur_zip = zipfile.ZipFile(file_name, 'r')
 install_steps = []
-for i in imgur_zip.namelist():
+for i in sorted(imgur_zip.namelist()):
     step = {
         'file':i,
         'install_to':'GameData/Squad/Flags',
@@ -37,6 +42,6 @@ for i in imgur_zip.namelist():
 
 ckan['install'] = install_steps
 
-json.dump(ckan, args.ckan_file, indent=1, sort_keys=True)
+json.dump(ckan, args.ckan_file, indent=1)
     
 
